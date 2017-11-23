e6f361120cbbdc10f8c156c3b6d0a7960f7b325b
mmck328 <edamame1010@hotmail.co.jp>
2017-11-24 02:18:37 +0900

    Update lectures/newsletters

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/ubiquitous-signage/hamster/server"	
-	// "github.com/ubiquitous-signage/hamster/lectures"
-	// "github.com/ubiquitous-signage/hamster/newsletters"
+	"time"
+	"github.com/ubiquitous-signage/hamster/server"
+	"github.com/ubiquitous-signage/hamster/lectures"
+	"github.com/ubiquitous-signage/hamster/newsletters"
 	// "github.com/ubiquitous-signage/hamster/portal"
 	// "github.com/ubiquitous-signage/hamster/schedules"
 	// "github.com/ubiquitous-signage/hamster/trains"
@@ -11,5 +12,10 @@ import (
 )
 
 func main() {
-	server.Run()
+	go server.Run()
+	go lectures.Run()
+	go newsletters.Run()
+	for {
+		time.Sleep(5000);
+	}
 }
