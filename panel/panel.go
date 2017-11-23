202b1525bbcf8c67ffbd28222df0835487d6a0a4
mmck328 <edamame1010@hotmail.co.jp>
2017-11-24 02:17:59 +0900

    Update Panel struct (devide PanelHeader & add Date field)

--- panel/panel.go
+++ panel/panel.go
@@ -1,12 +1,19 @@
 package panel
 
+import ("time")
+
 type Panel struct {
+	PanelHeader `bson:",inline"`
+	Contents interface{} `json:"contents"`
+}
+
+type PanelHeader struct {
 	Version  float64     `json:"version"`
 	Type     string      `json:"type"`
 	Title    string      `json:"title"`
 	Category string      `json:"category"`
-	Contents interface{} `json:"contents"`
-}
+	Date	 time.Time	 `json:"date"`
+ }
 
 type Content struct {
 	Type    string `json:"type"`
