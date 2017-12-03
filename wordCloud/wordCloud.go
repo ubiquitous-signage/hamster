7dd3ee2e4f69431a2c6698ccaf4411442001baaf
mmck328 <edamame1010@hotmail.co.jp>
2017-12-03 20:09:21 +0900

    Fix wordCloud.go (fixed error when collection "wordCloud" is empty)

--- wordCloud/wordCloud.go
+++ wordCloud/wordCloud.go
@@ -1,32 +1,32 @@
-
 package wordCloud
 
 import (
 	"log"
-	"net/http"	
+	"net/http"
 	"time"
+
+	"github.com/ant0ine/go-json-rest/rest"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"github.com/ant0ine/go-json-rest/rest"
 )
 
-type WordCloud struct{
-	Words []Word `json:"words"`
+type WordCloud struct {
+	Words           []Word `json:"words"`
 	WordCloudHeader `bson:",inline"`
 }
 
-type Word struct{
+type Word struct {
 	Text  string
 	Count int
 }
 
 type WordCloudHeader struct {
-	Version  float64     `json:"version"`
-	Type     string      `json:"type"`
-	Title    string      `json:"title"`
-	Category string      `json:"category"`
-	Date	   time.Time	 `json:"date"`
- }
+	Version  float64   `json:"version"`
+	Type     string    `json:"type"`
+	Title    string    `json:"title"`
+	Category string    `json:"category"`
+	Date     time.Time `json:"date"`
+}
 
 func PostWordCloud(w rest.ResponseWriter, r *rest.Request) {
 	wordCloud := WordCloud{}
@@ -54,31 +54,30 @@ func storeWordCloud(newWordCloud WordCloud) {
 	c := mgoSession.DB("ubiquitous-signage").C("wordCloud")
 
 	//update values
-	wordClouds := []WordCloud{}
-	c.Find(nil).All(&wordClouds)
-	wordCloud := wordClouds[0]
+	wordCloud := WordCloud{}
+	c.Find(nil).One(&wordCloud)
 
 	words := wordCloud.Words
 	newWords := newWordCloud.Words
 
-	for ni:= 0; ni < len(newWords); ni++ {
+	for ni := 0; ni < len(newWords); ni++ {
 		text := newWords[ni].Text
 
 		isEmerged := false
-		for i:= 0; i < len(words); i++ {
+		for i := 0; i < len(words); i++ {
 			// word := words[i]
 			// if text == word.Text {
 			// 	word.Count = word.Count + 1
 			// 	isEmerged = true
 			// 	break
-			// }  
+			// }
 			// word:= words[i]のwordの参照はwords[i]にはない？
 			// スライスとマップは参照型のはずなのだが
 			if text == words[i].Text {
 				words[i].Count = words[i].Count + 1
 				isEmerged = true
 				break
-			} 
+			}
 		}
 		if !isEmerged {
 			words = append(words, Word{text, 1})
@@ -94,15 +93,15 @@ func storeWordCloud(newWordCloud WordCloud) {
 		"version":  0.0,
 		"type":     "wordCloud",
 		"title":    "Word Cloud",
-		"category": "external", 
+		"category": "external",
 	}
 	mgoHeader := bson.M(fixedHeader)
 
-	wordCloud.Version  = fixedHeader["version"].(float64)
-	wordCloud.Type     = fixedHeader["type"].(string)
-	wordCloud.Title    = fixedHeader["title"].(string)
+	wordCloud.Version = fixedHeader["version"].(float64)
+	wordCloud.Type = fixedHeader["type"].(string)
+	wordCloud.Title = fixedHeader["title"].(string)
 	wordCloud.Category = fixedHeader["category"].(string)
-	wordCloud.Date     = time.Now()
+	wordCloud.Date = time.Now()
 
 	c.Upsert(
 		mgoHeader,
