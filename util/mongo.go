d707904375a753854d70230f76b1fc292084c8e3
mmck328 <edamame1010@hotmail.co.jp>
2017-12-09 02:32:58 +0900

    FIx config

--- util/mongo.go
+++ util/mongo.go
@@ -1,11 +1,11 @@
 package util
 
 import (
-	"gopkg.in/mgo.v2"
 	"github.com/spf13/viper"
+	"gopkg.in/mgo.v2"
 )
 
-func Con (endpoint string) *mgo.Session {
+func Con(endpoint string) *mgo.Session {
 	s, err := mgo.Dial(endpoint)
 	if err != nil {
 		panic(err)
@@ -13,22 +13,22 @@ func Con (endpoint string) *mgo.Session {
 	return s
 }
 
-func GetDB (DBName string, mongoEndpoint string) (s *mgo.Session, db *mgo.Database) {
+func GetDB(DBName string, mongoEndpoint string) (s *mgo.Session, db *mgo.Database) {
 	s = Con(mongoEndpoint)
 	db = s.DB(DBName)
 	return
 }
 
-func GetCollections (collectionName string, DBName string, mongoEndpoint string) (s *mgo.Session, c *mgo.Collection) {
+func GetCollections(collectionName string, DBName string, mongoEndpoint string) (s *mgo.Session, c *mgo.Collection) {
 	s, db := GetDB(DBName, mongoEndpoint)
 	c = db.C(collectionName)
 	return
 }
 
 func GetPanel() (s *mgo.Session, c *mgo.Collection) {
-	var mongoEndpoint string  = viper.GetString("mongo.endpoint")
-	var DBName string         = viper.GetString("mongo.DBName")
-	var collectionName string = "panels"
+	var mongoEndpoint = viper.GetString("mongo.endpoint")
+	var DBName = viper.GetString("mongo.DBName")
+	var collectionName = "panels"
 
 	s, c = GetCollections(collectionName, DBName, mongoEndpoint)
 	return
